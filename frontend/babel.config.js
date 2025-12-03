d57043a07548b0407367cb4702b360990e272925
prami25r <pramithir25@gmail.com>
2025-12-03 16:18:02 +0530

    feat: mobile-web

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,11 +1,3 @@
 module.exports = {
-  presets: [
-    "module:@react-native/babel-preset",
-    "@babel/preset-env",
-    "@babel/preset-react",
-  ],
-  plugins: [
-    ["babel-plugin-react-native-web", { commonjs: true }],
-    "@babel/plugin-transform-flow-strip-types",
-  ],
+  presets: ['module:@react-native/babel-preset'],
 };
